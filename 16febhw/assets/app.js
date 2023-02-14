ac5ba184affe2afdc545633d254fd47d386c0641
Amin <aminisrafilzadeh24@gmail.com>
2023-02-15 00:58:18 +0400

    homework added

---
+++ 16febhw/assets/app.js
@@ -0,0 +1,12 @@
+if(window.confirm("18 yasin var?")){
+    var age = prompt("tam olaraq nece")
+    if(age>=18){
+        
+    }
+    else{
+        document.location = 'https://www.instagram.com/tumurcuq_usaq_bagcasi/?hl=en';
+    }
+}
+else{
+    document.location = 'https://www.instagram.com/tumurcuq_usaq_bagcasi/?hl=en';
+}
